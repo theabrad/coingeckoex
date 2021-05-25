dc2bfe7014accca42f5b18bd223df1c5485c3fe9
Alex Bradley <alex.robert.bradley@gmail.com>
2021-05-25 01:14:13 -0400

    added endpoints for finance

---
+++ lib/coingecko/finance.ex
@@ -0,0 +1,20 @@
+defmodule Coingecko.Finance do
+  @doc """
+  List all finance platforms
+  Example:
+    iex> Coingecko.Finance.platforms
+    {:ok, {[...]}}
+  """
+  def platforms do
+    Request.one("finance_platforms")
+  end
+
+  @doc """
+  List all finance products
+  Example:
+    iex> Coingecko.Finance.products
+    {:ok, {[...]}}
+  """
+  def products do
+    Request.one("finance_products")
+end
